b278612015a5978070b72ebbf9d397a4e73705b7
Myatty <myintmyatps664@gmail.com>
2024-12-26 16:49:35 +0630

    Add Icon and drawer

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:feather_icons/feather_icons.dart';
 
 class Mainscreen extends StatefulWidget {
   const Mainscreen({super.key});
@@ -11,9 +12,18 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
-        title: Text('Todo App'),
         centerTitle: true,
+        title: Text('Lote Sa Yar'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.add,
+            ),
+          )
+        ],
       ),
     );
   }
